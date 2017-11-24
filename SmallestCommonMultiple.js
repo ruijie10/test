66a213d42a36ba921b0c4c8e25b7eadd05a37192
liruijie <happyruijie@outlook.com>
2017-11-24 18:23:55 +0800

    ‘找出能被两个给定参数和它们之间的连续数字整除的最小公倍数。’

---
+++ SmallestCommonMultiple.js
@@ -0,0 +1,52 @@
+/*找出能被两个给定参数和它们之间的连续数字整除的最小公倍数。
+
+范围是两个数字构成的数组，两个数字不一定按数字顺序排序。
+
+例如对 1 和 3 —— 找出能被 1 和 3 和它们之间所有数字整除的最小公倍数。
+ */
+function smallestCommons(arr) {
+    var cheng = 1;
+    var newarr = [];
+    var bei = [];
+    var gongbei = [];
+    var ax = 0;
+
+    arr.sort(function (a, b) {
+        return a - b;
+    });
+    for (var i = arr[0]; i <= arr[1]; i++) {
+        newarr.push(i);
+        cheng *= i;
+    }
+
+    for (var j = 1; j <= cheng; j++) {
+
+        bei.push(j);
+    }
+    function beishu(bei) {
+        for (var a = 0; a < newarr.length; a++) {
+
+            if (bei[b] % newarr[a] == 0) {
+                ax++;
+            } else {
+                ax = 0;
+                break;
+
+            }
+        }
+        return ax;
+    }
+
+    for (var b = 0; b < bei.length; b++) {
+
+        if (beishu(bei) === newarr.length) {
+            gongbei.push(bei[b]);
+            var zuixiao = gongbei[0];
+        }
+
+    }
+    return zuixiao;
+};
+
+
+smallestCommons([1, 3]);
