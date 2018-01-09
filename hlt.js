a466b0aab236620650405d7864eb508de17814e5
liruijie <happyruijie@outlook.com>
2018-01-09 22:27:11 +0800

    ‘flg’

--- hlt.js
+++ hlt.js
@@ -1,43 +1,35 @@
+
 $(document).ready(function () {
     $("#start").click(function () {
         $("#start").hide();
         $("#guan").show();
     });
     $("#determine").click(function () {
-        var n=$("#text").find("option:selected").text();
+        var n = $("#text").find("option:selected").text();
         $("#guan").hide();
         $("#hltshow").show();
-      /* if(b==1){
-        
-            $("#1a").show();
-            $("#2a").show();
+        for (var i = 1; i <= n; i++) {
+            $("#" + i + "a").show();
         }
-         if(n==2){
-            $("#hltshow").show();
-            $("#1a").show();
-            $("#2a").show();
-            $("#3a").show();
-         }
-         if(n==3){
-            $("#hltshow").show();
-            $("#1a").show();
-            $("#2a").show();
-            $("#3a").show();
-            $("#4a").show();
-         }
-         if(n==4){
-            $("#hltshow").show();
-            $("#1a").show();
-            $("#2a").show();
-            $("#3a").show();
-            $("#4a").show();
-            $("#5a").show();
-         }
-       */
+
+
     });
-    $("button").click(function () {
-       
-       
-       
+
+
+    $(".zhuzi").click(function () {
+        var flg = 1;
+        if (flg == 1) {
+            $(this).children("button:visible").first().css("border-color", "red");
+            var d = $(this).children("button:visible").first();
+            var x = $(this).children("button:visible").first().text();
+            var flg = 0;
+        } else {
+            $(d).hide();
+            var y = $(this)
+            $("#" + x + y[0]).show();
+
+        }
+
     });
+
 });
