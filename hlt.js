842bddac6524343c5f39bbe2c584688f7a11efd0
liruijie <happyruijie@outlook.com>
2018-01-10 12:25:53 +0800

    ‘完成后选择关卡没反应’

--- hlt.js
+++ hlt.js
@@ -2,30 +2,38 @@
 $(document).ready(function () {
     $("#start").click(function () {
         $("#start").hide();
-        $("#guan").show();
+        $("#guanka").show();
+    });
+    $(".next").click(function () {
+        $(".wancheng").hide();
+        $("#guanka").show();
+        i=1;
     });
     $("#determine").click(function () {
         var n = $("#text").find("option:selected").text();
-        $("#guan").hide();
-        $("#hltshow").show();
+        $("#guanka").hide();
+        
         for (var i = 1; i <= n; i++) {
-
+            $("#hltshow").show();
             $("#" + i + "a").show();
+            s = $(".hlta").children("button:visible").text();
         }
 
 
     });
-
+    var h = 0;
     var flg = 1;//判断柱子是否重复选择
     var f = 2;//判断柱子是否重复选择
     var d;//没有影藏的第一个 button 的 id；
-    var x=0;//没有影藏的第一个 button 的 text；
+    var x = 0;//没有影藏的第一个 button 的 text；
     var y;//柱子的序号 a,b,c 三根；
     var z;//上一次选择的柱子；
     var w;//当前选择的柱子 a;b;c；
     var m;//当前选择的 button 的 text；
-    
+    var s;//a 柱全部显示的 button；
+    var s1;//c柱全部显示的 button；
     $(".zhuzi").click(function () {
+
         w = $(this).children("h3").text()
         m = $(this).children("button:visible").first().text();
         if (z == w) {
@@ -51,20 +59,26 @@ $(document).ready(function () {
 
         }
         else {
-            if (x >m||m=="") {
+            if (x > m || m == "") {
                 $(d).hide();
                 y = $(this).children("h3").text()
                 $("#" + x + y).show();
+                s1 = $(this).children("button:visible").text();
                 flg = 1;
-                f=2;
-            }else{
+                f = 2;
+                if (s == s1) {
+                    $(".zhuzi").hide();
+                    $(".wancheng").show();
+                }
+            } else {
                 $("button").css("border-color", "black");
-                flg=1;
-                f=2;
+                flg = 1;
+                f = 2;
             }
 
         }
 
     });
+
 
 });
