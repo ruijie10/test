23e17cb0cb2706dbf7849f88974697eabee2e061
liruijie <happyruijie@outlook.com>
2018-01-17 22:21:56 +0800

    donghua

---
+++ SymmetricDifference.js
@@ -0,0 +1,22 @@
+function sym(args, args2) {
+    var arr = [];
+    var arry = [];
+
+    for (var i = 0; i < args.length; i++) {
+
+        if (args[i] == ) {
+            arry.push(args[i]);
+        }
+        arr.push(args[i]);
+
+    }
+
+
+
+
+    return arr;
+
+
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
