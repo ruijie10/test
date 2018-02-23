6314e1bf3aec2eb1bc02fae94bb3f0a06508e9e0
liruijie <happyruijie@outlook.com>
2018-02-23 23:04:33 +0800

    generate_find_map

--- generate_map.js
+++ generate_map.js
@@ -32,14 +32,12 @@ function generate_map(n, m) {
             }
         }
     }
-    for (var k = 0; k < map.length; k++) {
-        // console.log(map[i])
-        for (var a = 0; a < map[k].length; a++) {
-            document.write("[" + map[k][a] + "]," + "<br>");
-        }
-    }
+
+
     return map;
 
 
 }
-generate_map(10, 10)
+
+// generate_map(10, 10)
+module.exports = generate_map;
