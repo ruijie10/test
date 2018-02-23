7db34a5e966aa3ec8ed2376e155f311c19e7bf96
liruijie <happyruijie@outlook.com>
2018-02-23 19:43:25 +0800

    mi_gong

--- mi_gong.js
+++ mi_gong.js
@@ -52,121 +52,102 @@ function find_way(map) {
     var way = [];
     var flag = 0;
     var way2 = [];
+    var flag_map = new Array();
+    for (var i = 0; i <= map.length; i++) {
+        flag_map[i] = new Array();
+        for (var j = 0; j <= map[0].length; j++) {
+            flag_map[i][j] = true;
+        }
+    }
     function maze(map, x, y, x2, y2) {
         var x1,
             y1;
-        var w,
-            m;
-        var arr = [];
-        var repeat = 0;
         var room = map[x][y].split("");
         if (x == x2 && y == y2 && flag == 0) {
             way.push([x, y]);
             flag = 1;
-        }
-
-        for (var i = 0; i < room.length; i++) {
-            // 选择一个出口。
-            if (room[i] == 1 && flag == 0) {
-                // 路径保存在 way 中。
-                way.push([x, y]);
-                // 判断行走方向。
-                // 上
-                if (i == 0) {
-                    x1 = x - 1;
-                    y1 = y;
-                }
-                // 右上
-                else if (i == 1) {
-                    x1 = x - 1;
-                    y1 = y + 1;
-                }
-                // 右
-                else if (i == 2) {
-                    x1 = x;
-                    y1 = y + 1;
-                }
-                // 右下
-                else if (i == 3) {
-                    x1 = x + 1;
-                    y1 = y + 1;
-                }
-                // 下
-                else if (i == 4) {
-                    x1 = x + 1;
-                    y1 = y;
-                }
-                // 左下
-                else if (i == 5) {
-                    x1 = x + 1;
-                    y1 = y - 1;
-                }
-                // 左
-                else if (i == 6) {
-                    x1 = x;
-                    y1 = y - 1;
-                }
-                // 左上
-                else if (i == 7) {
-                    x1 = x - 1;
-                    y1 = y - 1;
-                }
-                // way 中数组转换为字符串。
-                for (var j = 0; j < way.length; j++) {
-                    arr.push(way[j].join(""));
-                }
-                // 下一个坐标转换成字符串
-                w = [x1, y1];
-                w = w.join("");
-                // 遍历路径 路径是否回头。
-                for (var k = 0; k < arr.length; k++) {
-                    if (w == arr[k]) {
-                        repeat = 1;
-                        break;
+            for (var i = 0; i < room.length; i++) {
+                // 选择一个出口。
+                if (room[i] == 1 && flag == 0 && flag_map[x][y]) {
+                    // 路径保存在 way 中。
+                    way.push([x, y]);
+                    flag_map[x][y] = false;
+                    // 判断行走方向。
+                    // 上
+                    if (i == 0) {
+                        x1 = x - 1;
+                        y1 = y;
                     }
-                    else {
-                        repeat = 0;
+                    // 右上
+                    else if (i == 1) {
+                        x1 = x - 1;
+                        y1 = y + 1;
                     }
-                }
-                if (repeat == 0) {
-                    maze(map, x1, y1, x2, y2);
-                }
-
-                if (flag == 0) {
-                    // 寻找下个出口 清除 way 最后的一个坐标
-                    way.pop();
-                    repeat = 0;
+                    // 右
+                    else if (i == 2) {
+                        x1 = x;
+                        y1 = y + 1;
+                    }
+                    // 右下
+                    else if (i == 3) {
+                        x1 = x + 1;
+                        y1 = y + 1;
+                    }
+                    // 下
+                    else if (i == 4) {
+                        x1 = x + 1;
+                        y1 = y;
+                    }
+                    // 左下
+                    else if (i == 5) {
+                        x1 = x + 1;
+                        y1 = y - 1;
+                    }
+                    // 左
+                    else if (i == 6) {
+                        x1 = x;
+                        y1 = y - 1;
+                    }
+                    // 左上
+                    else if (i == 7) {
+                        x1 = x - 1;
+                        y1 = y - 1;
+                    }
+                    if (flag_map[x1][y1]) {
+                        maze(map, x1, y1, x2, y2);
+                    }
+                    if (flag == 0) {
+                        // 寻找下个出口 清除 way 最后的一个坐标
+                        way.pop();
+                        flag_map[x][y] = true;
+                    }
                 }
             }
         }
+        maze(map, 0, 0, 10, 10);
+        for (var i = 0; i < way.length; i++) {
+            way2.push(way[i].join(","))
+        }
+        console.log("(" + way2.join(") -> (") + ")")
     }
-    maze(map, 0, 0, 2, 2);
+    console.time("map1")
+    find_way(map1);
+    console.timeEnd("map1");
+    console.log('\n')
 
-    for (var i = 0; i < way.length; i++) {
-        way2.push(way[i].join(","))
-    }
-    console.log("(" + way2.join(") -> (") + ")")
-}
-console.time("map1")
-find_way(map1);
-console.timeEnd("map1");
-console.log('\n')
+    console.time("map2");
+    find_way(map2);
+    console.timeEnd("map2");
+    console.log('\n')
 
-console.time("map2");
-find_way(map2);
-console.timeEnd("map2");
-console.log('\n')
+    console.time("map3");
+    find_way(map3);
+    console.timeEnd("map3");
+    console.log('\n')
 
-console.time("map3");
-find_way(map3);
-console.timeEnd("map3");
-console.log('\n')
-
-console.time("map4");
-find_way(map4);
-console.timeEnd("map4");
-
-
+    console.time("map4");
+    find_way(map4);
+    console.timeEnd("map4");
 
 
 
