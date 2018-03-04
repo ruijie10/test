e81848b1d8d16c22e1dbd7b159792359d786fd02
liruijie <happyruijie@outlook.com>
2018-03-04 16:03:52 +0800

    add_max

--- test_2/max.js
+++ test_2/max.js
@@ -1,42 +1,44 @@
-// function button(m, n) {
-//     // var m = document.getElementById("mun").value;
-//     // var n = parseInt(document.getElementById("plus").value)
-//     var flag_mun = [];
-//     var arr_mun = [];
-//     var arr = [];
-//     for (var i = 0; i < m.length; i++) {
-//         flag_mun[i] = true;
-//     }
-  
-//     function add(mun, n, x) {
-//         for (var j = 0; j < n; j++) {
-//             for (var i = 0; i < mun.length; i++) {
-//                 if (flag_mun[i]) {
-//                     var a = mun.slice(i, i + 1)
-//                     arr_mun.push(a);
-//                     flag_mun[i] = false;
-//                     flag++;
-//                     if (flag => n) {
-//                        arr= arr_mun;
-//                         add(mun, n, x + 1)
-//                         flag_mun[j] = true;
-//                         arr_mun.pop()
-//                     }
-//                     break;
-//                 }
-
-//             }
-//         }
-//         // }
-//         // b = parseInt(mun.slice(i, xx));
-//         // c += b;
-//         // flag += 1;
-//         // number1 = c;
-//         // m = parseInt(mun.slice(i + 1))
-//         return arr_mun
-//     }
-//     add(m, n, 0);
-//     $('#number').html(arr_mun);
-// }
-
-// button('12345', 3)
+function button(m, n) {
+    // var m = document.getElementById("mun").value;
+    // var n = parseInt(document.getElementById("plus").value)
+    var arr_mun = [];
+    var flag_mun = [];
+    var plus = 0;
+    for (var i = 0; i < m.length - 1; i++) {
+        flag_mun[i] = true;
+    }
+    function add(m, k, x) {
+        for (var i = 0; i < m.length - 1; i++) {
+            if (flag_mun[i] && k <= n) {
+                arr_mun.push(i);
+                for (let k = 0; k <= i; k++) {
+                    flag_mun[k] = false;
+                }
+                add(m, k + 1, x)
+                arr_mun.pop()
+                for (let k = m.length - 2; k > i; k--) {
+                    flag_mun[k] = true;
+                }
+            }
+        }
+        if (arr_mun.length == 3) {
+            var b = 0;
+            for (var j = 0; j < arr_mun.length; j++) {
+                var a = arr_mun[j]
+                if (j < arr_mun.length - 1) {
+                    b += parseInt(m.slice(j, a + 1))
+                }
+            }
+            b += parseInt(m.slice(a));
+            if (plus < b) {
+                plus = b;
+            }
+            console.log(b, arr_mun);
+        }
+    }
+    add(m, 1, 0)
+    console.log('\n');
+    console.log('\n');
+    console.log("最大和：" + plus);
+}
+button('1234567', 3)
--- test_2/max2.js
+++ test_2/max2.js
@@ -10,30 +10,32 @@ function button(m, n) {
     }
     function add(m, k, x) {
         for (var i = 0; i < m.length - 1; i++) {
-            if (flag_mun[i]) {
-                if (k > n) {
+            x++;
+            if (flag_mun[i] && x < m.length) {
+                if (k == n) {
+                    arr_mun.push(m.slice(i, x));
                     for (var j = 0; j < arr_mun.length; j++) {
                         a += parseInt(arr_mun[j])
                     }
-                    a += parseInt(m.slice(i));
+                    a += parseInt(m.slice(x));
                     if (plus < a) {
                         plus = a
                     }
                     return;
                 }
                 else {
-                    arr_mun.push(m.slice(i, i + x));
+                    arr_mun.push(m.slice(i, x));
                     flag_mun[i] = false;
                 }
 
                 add(m, k + 1, x)
                 arr_mun.pop()
                 flag_mun[i] = true;
-                x += 1;
+
             }
         }
     }
-    add(m, 1, 1)
+    add(m, 1, 0)
     return plus;
 }
 button('1234567', 3)
