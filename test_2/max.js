d51b1f5f04951811288b75291b828c8bf21a56a0
liruijie <happyruijie@outlook.com>
2018-03-04 17:51:26 +0800

    max_add

--- test_2/max.js
+++ test_2/max.js
@@ -4,46 +4,49 @@ function button(m, n) {
     var arr_mun = [];
     var flag_mun = [];
     var plus = 0;
-    for (var i = 0; i < m.length - 1; i++) {
+    for (let i = 0; i < m.length - 1; i++) {
         flag_mun[i] = true;
     }
     if (m.length <= n) {
         console.log("无最大和");
-        return ;
+        return;
     }
-
     function add(m, k) {
-        for (var i = 0; i < m.length - 1; i++) {
+        for (let i = 0; i < m.length - 1; i++) {
             if (flag_mun[i] && k <= n) {
                 arr_mun.push(i);
-                for (let k = 0; k <= i; k++) {
-                    flag_mun[k] = false;
+                for (let x = 0; x <= i; x++) {
+                    flag_mun[x] = false;
                 }
                 add(m, k + 1)
                 arr_mun.pop()
-                for (let k = m.length - 2; k > i; k--) {
-                    flag_mun[k] = true;
+                for (let y = m.length - 2; y > i; y--) {
+                    flag_mun[y] = true;
                 }
             }
         }
         if (arr_mun.length == n) {
             var b = 0;
-            for (var j = 0; j < arr_mun.length; j++) {
-                var a = arr_mun[j]
+            var a;
+            var c = '';
+            for (let j = 0; j < arr_mun.length; j++) {
+                a = arr_mun[j]
                 if (j < arr_mun.length - 1) {
+                    c += (m.slice(j, a + 1)) + '+';
                     b += parseInt(m.slice(j, a + 1))
+                } else {
+                    c += (m.slice(a)) + ' ='
                 }
             }
             b += parseInt(m.slice(a));
             if (plus < b) {
                 plus = b;
             }
-            console.log(b, arr_mun);
+            console.log(c, b);
         }
     }
     add(m, 1)
     console.log('\n');
-    console.log('\n');
     console.log("最大和：" + plus);
 }
-button('123567', 4)
+button('1234567', 4)
