14a920001cdbcf746987c2a10d9b99415f15df90
liruijie <happyruijie@outlook.com>
2018-03-06 19:21:48 +0800

    queen2

--- test_2/Queen2.js
+++ test_2/Queen2.js
@@ -1,74 +1,69 @@
-function queen_map(map) {
-    var arr = [];//选定的坐标数组
+function queen_map(n) {
+    var queen_arr = [];//选定的坐标数组
     var repeat_arr = new Array();//二维数组，每一层被改变标记的坐标
-    var flag_arr = new Array();//二维数组标记；
-    for (var i = 0; i < map; i++) {
-        flag_arr[[i]] = new Array();
-        for (var j = 0; j < map; j++) {
-            flag_arr[[i]][j] = true;
+    var map_flag = new Array();//二维数组标记；
+    var finish_flag = false;
+    var a = 0;
+    for (var i = 0; i < n; i++) {
+        map_flag[[i]] = new Array();
+        for (var j = 0; j < n; j++) {
+            map_flag[[i]][j] = true;
         }
     }
-    function queen(n, x) {// n 为 n*n 的 table ,  x 为每一层 当前为第0层；
+    function queen(x) {// x 为每一层 
+        var temp_ijs = [];
+        if (x == n) {
+            a += 1;
+            finish_flag = true;
+            console.log(queen_arr)
+            return;
+        }
         //循环遍历第 x 层
         for (var y = 0; y < n; y++) {
-            var arr1 = [];
-            if (x < n) {
-                if (flag_arr[x][y] == true) {
-                    arr.push(y);//选定坐标放入 arr
-
-                    // 纵行标记
-                    for (let i = x; i < n; i++) {
-                        if (flag_arr[i][y] == true) {
-                            arr1.push([i, y])
-                        }
-                        flag_arr[i][y] = false;
+            if (map_flag[x][y] == true) {
+                queen_arr.push(y);//选定坐标放入 queen_arr
+                // 纵行标记
+                for (let i = x; i < n; i++) {
+                    if (map_flag[i][y] == true) {
+                        temp_ijs.push([i, y])//当前做变更的标记坐标 true ==> flase
+                        map_flag[i][y] = false;
                     }
-                    // 左斜下方标记
-                    if (y != 0 && x != n - 1) {
-                        for (let a = x, b = y; a < n && b > 0; a++ , b--) {
-                            if (flag_arr[a][b] == true) {
-                                arr1.push([a, b])//当前做变更的标记坐标 true ==> flase
-                                flag_arr[a][b] = false;
-                            }
-                        }
-                    }
-                    // 左斜下方标记
-                    if (y != n - 1 && x != n - 1) {
-                        for (let a = x, b = y; a < n && b < n; a++ , b++) {
-                            if (flag_arr[a][b] == true) {
-                                arr1.push([a, b])//当前做变更的标记坐标 true ==> flase
-                                flag_arr[a][b] = false;
-                            }
-                        }
-                    }
-                    //当前做变更的标记坐标 true ==> flase 放入 x 层
-                    repeat_arr[x] = new Array();
-                    for (let i = 0; i < arr1.length; i++) {
-                        repeat_arr[x].push(arr1[i]);
-
-                    }
-
-                    // 进入下一层
-                    queen(n, x + 1);
-                    // 提取上一层 x 标记坐标， flase变成 true
-                    if (x == n - 1 && flag_arr[x][y] == true) {
-                        return;
-                    }
-                    for (let i = 0; i < repeat_arr[x].length; i++) {
-                        var arr2 = repeat_arr[x][i];
-                        var arrx = arr2[0];
-                        var arry = arr2[1];
-                        flag_arr[arrx][arry] = true;
-                    }
-                    arr.pop();//删除上一层选择的坐标
-                    repeat_arr.pop();//删除上一层 true 改变成 flase 的坐标
                 }
+                // 左斜下方标记
+                if (y != 0 && x != n - 1) {
+                    for (let i = x, j = y; i < n && j > 0; i++ , j--) {
+                        if (map_flag[i][j] == true) {
+                            temp_ijs.push([i, j])//当前做变更的标记坐标 true ==> flase
+                            map_flag[i][j] = false;
+                        }
+                    }
+                }
+                // 左斜下方标记
+                if (y != n - 1 && x != n - 1) {
+                    for (let i = x, j = y; i < n && j < n; i++ , j++) {
+                        if (map_flag[i][j] == true) {
+                            temp_ijs.push([i, j])//当前做变更的标记坐标 true ==> flase
+                            map_flag[i][j] = false;
+                        }
+                    }
+                }
+                // 进入下一层
+                queen(x + 1);
+                // 提取上一层 x 标记坐标， flase变成 true
+                // if (finish_flag) {
+                //     return;
+                // }
+                for (let i = 0; i < temp_ijs.length; i++) {
+                    map_flag[temp_ijs[i][0]][temp_ijs[i][1]] = true;
+                }
+                queen_arr.pop();//删除上一层选择的坐标
+
             }
         }
-        if (arr.length == n) {
-            console.log(arr)
-        }
+
     }
-    queen(map, 0)
+    queen(0)
+    console.log('一共有：' + a + '个')
+    console.log(queen_arr)
 }
 queen_map(8)
