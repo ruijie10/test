0fc55ee5bc5069bb791310c97fa0d36d488c0b3e
rallets <stellar@rallets.com>
2018-12-11 15:30:50 +0800

    指针

---
+++ C-Language/hello/pointer3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+  int  var = 20;   /* 实际变量的声明 */
+  int  *ip;        /* 指针变量的声明 */
+  ip = &var;  /* 在指针变量中存储 var 的地址 */
+  int  *ptr = NULL; /*声明空指针 */
+  int *sb = &var;
+  printf("var变量的地址: %p\n", &var  );
+  printf("ip在指针变量中存储的地址: %p\n", ip );
+  printf("*ip指针变量的值: %d\n", *ip );
+  printf("ptr是空指针 它的地址是 %p\n", ptr  );
+  printf("*sb指针变量的值: %d\n", *sb );
+  /* printf("sb在指针变量中存储的地址: %d\n", sb ); */
+  return 0;
+}
